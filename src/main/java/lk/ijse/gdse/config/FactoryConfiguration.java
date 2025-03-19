c4ebb03d0cd181844baabd0b31de6657c21f541a
sasuniii0 <sasuniwijerathne@gmail.com>
2025-03-19 14:38:28 +0530

    configuration

--- src/main/java/lk/ijse/gdse/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/gdse/config/FactoryConfiguration.java
@@ -1,12 +1,8 @@
 package lk.ijse.gdse.config;
 
-import lk.ijse.gdse.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
 import java.io.IOException;
 import java.util.Properties;
 
@@ -21,7 +17,7 @@ public class FactoryConfiguration {
        //add property
        Properties properties = new Properties();
 
-       //add already created hibernate file to properies in current thread
+       //add already created hibernate file to properties in current thread
        try {
            properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));   //set path file is found no matter where the code is running
        } catch (IOException e) {
@@ -30,9 +26,6 @@ public class FactoryConfiguration {
 
        //add properties to configure
        configuration.setProperties(properties);
-
-       //add annotated class to configure
-       configuration.addAnnotatedClass(Student.class);
 
        //build session factory
        sessionFactory = configuration.buildSessionFactory();

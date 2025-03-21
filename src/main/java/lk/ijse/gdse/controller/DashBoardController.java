b3f37c9e7d2756f86703e906c866d21e57060048
sasuniii0 <sasuniwijerathne@gmail.com>
2025-03-21 10:05:17 +0530

    fxml

--- src/main/java/lk/ijse/gdse/controller/DashBoardController.java
+++ src/main/java/lk/ijse/gdse/controller/DashBoardController.java
@@ -1,4 +1,84 @@
 package lk.ijse.gdse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
 public class DashBoardController {
+
+    @FXML
+    private Button BtnHome;
+
+    @FXML
+    private Button BtnLogOut;
+
+    @FXML
+    private Button BtnPatient;
+
+    @FXML
+    private Button BtnPayment;
+
+    @FXML
+    private Button BtnSessionApp;
+
+    @FXML
+    private Button BtnTherapist;
+
+    @FXML
+    private Button BtnTherapyPrograms;
+
+    @FXML
+    private Button BtnUser;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private AnchorPane sideAncPane;
+
+    @FXML
+    private Pane sidePane;
+
+    @FXML
+    void BtnHomeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnLogOutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnPatientOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnPaymentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnSessionAppOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnTherapistOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnTherapyProgramsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void BtnUserOnAction(ActionEvent event) {
+
+    }
+
 }
